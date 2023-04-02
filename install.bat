f5e75680c7e4dd56721feeca911e736840c1ed3e
NOTGTM <107492638+NOTGTM@users.noreply.github.com>
2023-04-02 19:18:58 +0200

    Delete install.bat

--- install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@title installer Chess.Com Stat Tracker & Auto q
-pip install -r requirements.txt
-cls
-echo Starting Stat Tracker if it opens a vsc window or anything just close it and open the STATS-TRACKER.py file normally
-START STATS-TRACK.PY
-echo installed click enter to exit!
-pause -nul
